e9b7fdf501d5ba65d253f107b3def1284e6bcd38
s16217@pjwstk.edu.pl <#Pjwstk888>
2021-01-29 23:55:16 +0100

    example

--- example.sh
+++ example.sh
@@ -1,29 +1,11 @@
 #!/usr/bin/env bash
-set -euo pipefail
 source bach.sh
 
 test-rm-rf() {
-    # Write your test case
-
     project_log_path=/tmp/project/logs
-    sudo rm -rf "$project_log_ptah/" # Typo here!
+    sudo rm -rf "$project_log_ptah/" # Typo!
+    # An undefined bash variable is an empty string, which can be a serious problem!
 }
 test-rm-rf-assert() {
-    # Verify your test case
-    sudo rm -rf /   # This is the actual command to run on your host!
-                    # DO NOT PANIC! By using Bach Testing Framework it won't actually run.
+    sudo rm -rf /
 }
-
-test-rm-your-dot-git() {
-    # Mock `find` command with certain parameters, will output two directories
-
-    @mock find ~ -type d -name .git === @stdout ~/src/your-awesome-project/.git \
-                                                ~/src/code/.git
-
-    # Do it, remove all .git directories
-    find ~ -type d -name .git | xargs -- rm -rf
-}
-test-rm-your-dot-git-assert() {
-    # Verify the actual command
-
-    rm -rf ~/src/your-awesome-project/.git ~/src/code/.git
