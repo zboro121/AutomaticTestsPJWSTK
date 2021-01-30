18cf95f29faf2b390baf5771da1945a3623f865e
zboro121 <61629256+zboro121@users.noreply.github.com>
2021-01-30 18:40:54 +0100

    Update example.sh

--- example.sh
+++ example.sh
@@ -3,7 +3,7 @@ source bach.sh
 
 test-rm-rf() {
     project_log_path=/tmp/project/logs
-    sudo rm -rf "$project_log_path/" # Typo!
+    sudo rm -rf "$project_log_ptah/" # Typo!
     # An undefined bash variable is an empty string, which can be a serious problem!
 }
 test-rm-rf-assert() {
